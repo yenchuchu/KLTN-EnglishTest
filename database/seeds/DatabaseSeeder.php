2ecd9d41f2142e1eeac8dfa9f4085e9883124381
thuyhien <yenchuchu95@gmail.com>
2017-03-16 00:13:32 +0700

    tạo dữ liệu fake cho listen. - việc tiếp theo: cho hiện thị phần giao diện listen của học sinh.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UsersTableSeeder::class);
-//         $this->call(AnswerquestionsTableSeeder::class);
-//         $this->call(TickTrueFalseTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+         $this->call(AnswerquestionsTableSeeder::class);
+         $this->call(TickTrueFalseTableSeeder::class);
          $this->call(ListenCompleteSentencesTableSeeder::class);
          $this->call(ListenTableTicksTableSeeder::class);
          $this->call(ListenTableMatchsTableSeeder::class);
